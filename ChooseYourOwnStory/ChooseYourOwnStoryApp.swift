edcb77dc12b203cd356b7d0c2c40098b1e612215
Alex <oleksii.pidhornyi@gmail.com>
2024-12-17 12:00:23 +0100

    Initial Commit

---
+++ ChooseYourOwnStory/ChooseYourOwnStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseYourOwnStoryApp.swift
+//  ChooseYourOwnStory
+//
+//  Created by Oleksii on 17.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChooseYourOwnStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
